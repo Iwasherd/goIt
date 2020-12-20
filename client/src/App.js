b32ab4972889c69dcc78998fa27ce969fdb73282
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-12-20 12:43:01 +0200

    Added input and dropzone

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import Input from './components/Input/Input';
+import Dropzone from './components/Dropzone/Dropzone';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Input />
+      <Dropzone />
     </div>
   );
 }
