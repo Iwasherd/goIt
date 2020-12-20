17238aa72010dedab930aefa49b6c82faf390242
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-12-20 12:44:57 +0200

    Added input and dropzone

---
+++ client/src/components/Dropzone/Dropzone.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dropzone = () => {
+  return (
+    <div style={{ height: '300px', width: '300px', border: '1px solid #ccc' }}>
+      <p>Drop file here</p>
+    </div>
+  );
+};
+
+export default Dropzone;
---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input = () => {
+  return <input type="file" name="upload" />;
+};
+
+export default Input;
